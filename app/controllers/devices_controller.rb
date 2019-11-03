a83298667eb223417d25fa2e0463455df544dc15
HenryVexZHou <43188301+NeverLucky123@users.noreply.github.com>
2019-11-03 15:23:33 -0800

    email templates updated

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -4,7 +4,13 @@ class DevicesController < ApplicationController
 		@owner_email = params[:owner_email]
 		@device_name = params[:device_name]
 		@error_message = params[:error_message]
-		AlertMailer.with(owner_email: @owner_email, device_name: @device_name, error_message: @error_message).alert.deliver_now
+    @error_type = params[:error_type]
+    @url = request.original_url
+		AlertMailer.with(owner_email: @owner_email, 
+                     device_name: @device_name, 
+                     error_type: @error_type,
+                     error_message: @error_message,
+                     url: @url).alert.deliver_now
   end
 
   def RequestPredict	
--- app/mailers/alert_mailer.rb
+++ app/mailers/alert_mailer.rb
@@ -2,9 +2,12 @@ class AlertMailer < ApplicationMailer
 		default from: 'henryzhou000@gmail.com'
 
 	def alert
+		@time=Time.now.getutc
 		@owner_email = params[:owner_email]
 		@device_name = params[:device_name]
 		@error_message = params[:error_message]
-		mail(to: @owner_email, subject: @error_message)
+		@error_type = params[:error_type]
+		@url = params[:url]
+		mail(to: @owner_email, subject: "Autonomous Trash Can "+@device_name+" threw a "+@error_type+" error")
 	end
 end
