b49d0e68f0db3b08dc420f8633b9d3e73ae54e7d
HenryVexZHou <43188301+NeverLucky123@users.noreply.github.com>
2019-12-30 22:40:08 -0800

    updated mailer

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -5,8 +5,12 @@ class DevicesController < ApplicationController
 		@device_name = params[:device_name]
 		@error_message = params[:error_message]
     @error_type = params[:error_type]
+    @time = params[:time]
+    @time_zone = params[:time_zone]
     @url = request.original_url
 		AlertMailer.with(owner_email: @owner_email, 
+                     time: @time,
+                     time_zone: @time_zone,
                      device_name: @device_name, 
                      error_type: @error_type,
                      error_message: @error_message,
--- app/mailers/alert_mailer.rb
+++ app/mailers/alert_mailer.rb
@@ -2,12 +2,13 @@ class AlertMailer < ApplicationMailer
 		default from: 'henryzhou000@gmail.com'
 
 	def alert
-		@time=Time.now.getutc
+		@time=params[:time]
+		@time_zone=params[:time_zone]
 		@owner_email = params[:owner_email]
 		@device_name = params[:device_name]
 		@error_message = params[:error_message]
 		@error_type = params[:error_type]
 		@url = params[:url]
-		mail(to: @owner_email, subject: "Autonomous Trash Can "+@device_name+" threw a "+@error_type+" error")
+		mail(to: @owner_email, subject: "One of your TRASHSORT devices experienced an error")
 	end
 end
